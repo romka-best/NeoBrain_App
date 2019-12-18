787ff61d0ad94cabadfb5018e68efd802d4513d7
romka-best <romka.best2016@yandex.ru>
2019-12-18 22:33:31 +0300

    Delete error

--- app/src/main/java/com/example/neobrain/MainActivity.java
+++ app/src/main/java/com/example/neobrain/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.neobrain;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 }
