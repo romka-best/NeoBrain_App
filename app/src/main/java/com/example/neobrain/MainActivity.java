b642630f3a1670c2581cdf96406518d24a96e413
romka-best <romka.best2016@yandex.ru>
2020-02-09 01:05:50 +0300

    Add Conductor

--- app/src/main/java/com/example/neobrain/MainActivity.java
+++ app/src/main/java/com/example/neobrain/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.neobrain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.bluelinelabs.conductor.Conductor;
+import com.bluelinelabs.conductor.Router;
+import com.bluelinelabs.conductor.RouterTransaction;
+
+public class MainActivity extends AppCompatActivity{
+    private Router router;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewGroup container = (ViewGroup) findViewById(R.id.controller_container);
+        router = Conductor.attachRouter(this, container, savedInstanceState);
+        if (!router.hasRootController()) {
+            router.setRoot(RouterTransaction.with(new HomeController()));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!router.handleBack()) {
+            super.onBackPressed();
+        }
     }
 }
