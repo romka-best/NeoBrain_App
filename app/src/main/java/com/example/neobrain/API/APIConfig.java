88b524bbbcf19cc12eb3ec06eb1f282e86092338
romka-best <romka.best2016@yandex.ru>
2020-03-03 00:33:52 +0300

    delete files

--- app/src/main/java/com/example/neobrain/API/APIConfig.java
+++
@@ -1,6 +0,0 @@
-package com.example.neobrain.API;
-
-public class APIConfig {
-    public static final String BASE_URL = "";
-    public static final String FORMAT = ".json";
-}
--- app/src/main/java/com/example/neobrain/DB/AppDB.java
+++
@@ -1,9 +0,0 @@
-package com.example.neobrain.DB;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-//@Database(entities = {Task.class}, version = 1)
-public abstract class AppDB extends RoomDatabase {
-//    public abstract TaskDao taskDao();
-}
